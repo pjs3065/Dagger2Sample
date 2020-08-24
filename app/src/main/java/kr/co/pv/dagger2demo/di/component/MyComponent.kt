e65c1061e666e1e29bc5f653408fe4bfb60742d2
pjs3065 <tmakdlfwotjd@naver.com>
2020-08-24 09:20:32 +0900

    상속된 클래스에 의존성 주입하기 설명추가

--- app/src/main/java/kr/co/pv/dagger2demo/di/component/MyComponent.kt
+++ app/src/main/java/kr/co/pv/dagger2demo/di/component/MyComponent.kt
@@ -7,5 +7,5 @@ import kr.co.pv.dagger2demo.di.module.MyModule
 @Component(modules = [MyModule::class])
 interface MyComponent {
     fun getString():String //프로비전 메서드, 바인드된 모듈로부터 의존성 제공
-    fun inject(selfClass: SelfClass)
+    fun inject(selfClass: SelfClass) //멤버 인젝션 메소드의 파라미터값 이상부터 주입이 시작
 }
